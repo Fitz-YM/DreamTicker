4edc4c3b21be8ac1dada71bfaf103e03d1b4ec93
Fitz-YM <85378561+Fitz-YM@users.noreply.github.com>
2024-02-20 14:54:26 +0800

    Unity 版本更改
    Unity 版本更改

--- Assets/Scripts/BlockCategory.cs
+++ Assets/Scripts/BlockCategory.cs
@@ -1,6 +1,18 @@
+/// <summary>
+/// 区块类别
+/// </summary>
 public enum BlockCategory
 {
+    /// <summary>
+    /// 在镜子前
+    /// </summary>
     BeforeMirror,
+    /// <summary>
+    /// 在镜子中
+    /// </summary>
     InMirror,
+    /// <summary>
+    /// 在镜子后
+    /// </summary>
     BehindMirror,
 }
